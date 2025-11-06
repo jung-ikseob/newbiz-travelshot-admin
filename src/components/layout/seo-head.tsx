4f90146a294bb929f58b8018aecf5b2687b0e710
정익섭 <ikseob_jung@kolon.com>
2025-11-06 16:38:11 +0900

    .

--- src/components/layout/seo-head.tsx
+++ src/components/layout/seo-head.tsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import React from "react";
 
-export const DEFAULT_TITLE = "Purple Admin UI";
-export const DEFAULT_DESCRIPTION = "Purple Admin UI";
+export const DEFAULT_TITLE = "Travel Shot Admin UI";
+export const DEFAULT_DESCRIPTION = "Travel Shot Admin UI";
 
 interface ISeoHeadProps {
   title?: string;
